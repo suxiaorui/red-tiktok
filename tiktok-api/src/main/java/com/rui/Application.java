21a7d3f5d1851779b337a97668ed7706790839d3
suxiaorui <aaa1097395576@foxmail.com>
2023-07-28 09:34:28 +0800

    init

---
+++ tiktok-api/src/main/java/com/rui/Application.java
@@ -0,0 +1,18 @@
+package com.rui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author suxiaorui
+ * @Description TODO
+ * @Date 2023/7/28 9:23
+ * @Version 1.0
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}

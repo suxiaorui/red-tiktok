30500fa29963920a9e0b9ba3c6847b777d2668c1
suxiaorui <aaa1097395576@foxmail.com>
2023-07-28 10:16:58 +0800

    配置knife4j实现文档接口

--- tiktok-api/src/main/java/com/rui/controller/HelloController.java
+++ tiktok-api/src/main/java/com/rui/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.rui.controller;
 
 import com.rui.grace.result.GraceJSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version 1.0
  */
 
+@Slf4j
+@Api(tags = "Hello 测试的接口")
 @RestController
 public class HelloController {
 
+    @ApiOperation(value = "hello-这是一个hello的测试路由")
     @GetMapping("hello")
     public Object hello(){
         return GraceJSONResult.ok("hello");

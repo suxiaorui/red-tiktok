9c5a2ab8448c95b165d4398c7b8b06a9b34f45db
suxiaorui <aaa1097395576@foxmail.com>
2023-07-28 16:52:23 +0800

    异常封装返回

---
+++ tiktok-common/src/main/java/com/rui/exceptions/GraceException.java
@@ -0,0 +1,15 @@
+package com.rui.exceptions;
+
+
+import com.rui.grace.result.ResponseStatusEnum;
+
+/**
+ * 优雅的处理异常，统一封装
+ */
+public class GraceException {
+
+    public static void display(ResponseStatusEnum responseStatusEnum) {
+        throw new MyCustomException(responseStatusEnum);
+    }
+
+}
